bae9679cd8493993612eb1cb147180aea7b6671a
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-02-23 12:37:49 +0000

    Ready to deploy

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'wo7=p2cc(kabj80^sj11*x)*10p-&q=_85a_2e0tgw#dad*yb$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
