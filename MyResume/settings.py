a404c13ec30c9749293798f23b6b737f29e719d8
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-02-23 14:18:39 +0000

    Ready to deploy

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -130,14 +130,16 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+
 
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'staticfiles/'),
 # )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 
 # Default primary key field type
