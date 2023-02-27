9c84e324cc0cdc60aed719ac695cd4e6b835bcb8
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-02-27 06:52:29 +0100

    Update settings.py

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -33,6 +33,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-196eb.up.railway.app',]
 
+if 'CODESPACE_NAME' in os.environ:
+    codespace_name = os.getenv("CODESPACE_NAME")
+    codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
+    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
+
 
 # Application definition
 
